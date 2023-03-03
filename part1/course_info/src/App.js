2c0e3aac972ebb207be0cb22c25d19bc840a47e1
amisha <coderamisha@gmail.com>
2023-03-03 16:13:27 +0530

    added part1

---
+++ part1/course_info/src/App.js
@@ -0,0 +1,46 @@
+const User = (user) => {
+  return (
+    <div>
+      {user.name} is learning React!
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts.parts} />
+      <Footer />
+    </div>
+  )
+}
+
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/Amisha-100'>amisha</a>
+    </div>
+  )
+}
+
+export default App
